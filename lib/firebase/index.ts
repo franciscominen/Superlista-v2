e75a8d5df9f251ac58e1acca5480986fc87d933c
Francisco Minen <fran.minen@gmail.com>
2022-10-05 17:13:35 -0300

    Try to fix build

--- lib/firebase/index.ts
+++ lib/firebase/index.ts
@@ -1,7 +1,14 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
-const firebaseConfig: string = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG ?? '')
+const firebaseConfig = {
+    apiKey: "AIzaSyArdkMYgTUwCXX70lB11KWAttnxDQ8zquQ",
+    authDomain: "lista-super-app.firebaseapp.com",
+    projectId: "lista-super-app",
+    storageBucket: "lista-super-app.appspot.com",
+    messagingSenderId: "435567422333",
+    appId: "1:435567422333:web:015bbebddc89e553e63a14"
+}
 
 !firebase.apps.length && firebase.initializeApp(firebaseConfig)
 
